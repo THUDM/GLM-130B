6615fdd0721339a689808ffcde05fa3f5155c7b5
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2022-11-17 23:03:04 +0800

    Add GLM-Base model

--- configs/model_glm_base.sh
+++ configs/model_glm_base.sh
@@ -1,12 +1,12 @@
 MODEL_TYPE="glm-large-generation"
-CHECKPOINT_PATH="/zhangpai21/checkpoints/glm-large-en-generation"
+CHECKPOINT_PATH="/zhangpai21/checkpoints/glm-base-en-blank"
 MP_SIZE=1
 MODEL_ARGS="--model-parallel-size ${MP_SIZE} \
             --vocab 30592 \
-            --num-layers 24 \
-            --hidden-size 1024 \
-            --num-attention-heads 16 \
+            --num-layers 12 \
+            --hidden-size 768 \
+            --num-attention-heads 12 \
             --max-sequence-length 513 \
             --tokenizer-type glm_BertWordPieceTokenizer \
-            --tokenizer-model-type bert-large-uncased \
+            --tokenizer-model-type bert-base-uncased \
             --load ${CHECKPOINT_PATH}"
